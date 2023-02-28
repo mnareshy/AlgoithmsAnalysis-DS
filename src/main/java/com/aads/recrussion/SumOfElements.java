8bc7c9f1ae855fae1adfb47991d02c49912ec7f6
Naresh M <mnaresh.y@gmail.com>
2023-02-28 12:54:21 +0530

    a

---
+++ src/main/java/com/aads/recrussion/SumOfElements.java
@@ -0,0 +1,27 @@
+package com.aads.recrussion;
+
+public class SumOfElements {
+
+    static   int  x =0;
+
+    public static int sumEle(int n){
+
+        x++;
+
+        if(n < 0)
+
+            return  0;
+        else
+            return sumEle(n-1)+n;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(sumEle(12) +" "+x);
+        System.out.println(sumEle(12) +" "+x);
+
+    }
+
+}
